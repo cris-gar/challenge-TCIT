b7e60771fa2bf3b6db335713739770411aedff5e
cris-gar <cristian.garrido@ceinf.cl>
2020-07-15 12:59:16 -0400

    remove data sensitive

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,8 +1,9 @@
-{
+require('dotenv').config();
+module.exports = {
   "development": {
-    "username": "crisgar",
-    "password": "Finisterra01",
-    "database": "entrevista",
+    "username": process.env.POSTGRES_USER,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DB,
     "host": "postgres",
     "dialect": "postgres"
   },
